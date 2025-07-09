63a8257329ebbc9c54a6366832c30364ec4610c4
IsFrydag <freitagheiner3@gmail.com>
2025-07-09 11:31:40 +0200

    Creation of project and initial commit.

---
+++ src/main/java/groupv16/prg381/java/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package groupv16.prg381.java;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
