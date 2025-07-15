2eb1480b0847d24831dbf685687c9d2fe7b3fcb0
IsFrydag <freitagheiner3@gmail.com>
2025-07-15 22:46:59 +0200

    Fixed some CSS, fixed other issues

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -12,10 +12,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-/**
- *
- * @author jana
- */
+
 @WebServlet(urlPatterns = {"/LogoutServlet"})
 public class LogoutServlet extends HttpServlet {
 
